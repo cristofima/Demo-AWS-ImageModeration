bed848f0915ba10ea045239ddc41a256303d2118
Cristopher Coronado <cristofima@hotmail.com>
2025-02-28 16:20:04 -0500

    refactor(UI): useTransition for isUploading and update to React 19

--- UI/src/components/Upload/Upload.tsx
+++ UI/src/components/Upload/Upload.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useTransition } from "react";
 import { toast } from "react-toastify";
 import apiService from "../../services/api-service";
 import "./Upload.css";
@@ -6,37 +6,36 @@ import { AxiosError } from "axios";
 
 const Upload = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const [isUploading, setIsUploading] = useState(false);
+  const [isUploading, startTransition] = useTransition();
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0] || null;
     setSelectedFile(file);
   };
 
-  const handleUpload = async () => {
+  const handleUpload = () => {
     if (!selectedFile) {
       toast.error("No file selected");
       return;
     }
 
-    const formData = new FormData();
-    formData.append("image", selectedFile);
-    setIsUploading(true);
-
-    try {
-      await apiService.uploadImage(formData);
-      setSelectedFile(null);
-      (document.getElementById("file-input") as HTMLInputElement).value = "";
-      toast.success("Image uploaded successfully");
-    } catch (e: unknown) {
-      if (e instanceof AxiosError && e?.response?.data?.message) {
-        toast.error(e.response.data.message);
-      } else {
-        toast.error("Failed to upload image");
+    startTransition(async () => {
+      const formData = new FormData();
+      formData.append("image", selectedFile);
+      
+      try {
+        await apiService.uploadImage(formData);
+        setSelectedFile(null);
+        (document.getElementById("file-input") as HTMLInputElement).value = "";
+        toast.success("Image uploaded successfully");
+      } catch (e: unknown) {
+        if (e instanceof AxiosError && e?.response?.data?.message) {
+          toast.error(e.response.data.message);
+        } else {
+          toast.error("Failed to upload image");
+        }
       }
-    } finally {
-      setIsUploading(false);
-    }
+    });
   };
 
   return (
